3acd1a63b64b2a18b76927c4bdac9e36dd799b69
ChisKien <chiskien03214@gmail.com>
2023-05-08 22:40:22 +0700

    update project properties

--- src/main/resources/sql_scripts/db.sql
+++ src/main/resources/sql_scripts/db.sql
@@ -1,7 +1,6 @@
 create table genre
 (
     genre_id   int unsigned auto_increment
-        constraint `PRIMARY`
         primary key,
     genre_name int null
 );
@@ -9,7 +8,6 @@ create table genre
 create table publisher
 (
     publisher_id int unsigned auto_increment
-        constraint `PRIMARY`
         primary key,
     name         varchar(255)                 not null,
     homepage     varchar(100)                 null,
@@ -19,7 +17,6 @@ create table publisher
 create table status
 (
     status_id   int unsigned auto_increment
-        constraint `PRIMARY`
         primary key,
     status_name varchar(50) not null
 );
@@ -27,7 +24,6 @@ create table status
 create table type
 (
     type_id   int unsigned auto_increment
-        constraint `PRIMARY`
         primary key,
     type_name varchar(50) not null
 );
@@ -35,7 +31,6 @@ create table type
 create table book
 (
     id              int unsigned auto_increment
-        constraint `PRIMARY`
         primary key,
     title           varchar(255) charset utf8mb3 not null,
     cover           varchar(255)                 null,
@@ -62,8 +57,8 @@ create table book_genre
 (
     book_id  int unsigned not null,
     genre_id int unsigned not null,
-    constraint `PRIMARY`
-        primary key (book_id, genre_id),
+
+    primary key (book_id, genre_id),
     constraint books_genres_books_id_fk
         foreign key (book_id) references book (id),
     constraint books_genres_genres_genre_id_fk
@@ -74,8 +69,8 @@ create table book_in_bookshelf
 (
     bookshelf_id int unsigned not null,
     book_id      int unsigned not null,
-    constraint `PRIMARY`
-        primary key (bookshelf_id, book_id),
+
+    primary key (bookshelf_id, book_id),
     constraint book_in_bookshelf_book_id_fk
         foreign key (book_id) references book (id)
 );
@@ -83,7 +78,6 @@ create table book_in_bookshelf
 create table user
 (
     user_id       int unsigned auto_increment
-        constraint `PRIMARY`
         primary key,
     first_name    varchar(255) charset utf8mb3         not null,
     middle_name   varchar(255) charset utf8mb3         null,
@@ -103,8 +97,8 @@ create table book_author
 (
     book_id   int unsigned not null,
     author_id int unsigned not null,
-    constraint `PRIMARY`
-        primary key (book_id, author_id),
+
+    primary key (book_id, author_id),
     constraint book_author_book_id_fk
         foreign key (book_id) references book (id)
             on delete cascade,
@@ -116,7 +110,6 @@ create table book_author
 create table book_shelf
 (
     bookshelf_id   int unsigned auto_increment
-        constraint `PRIMARY`
         primary key,
     user_id        int unsigned                 not null,
     bookshelf_name varchar(100)                 not null,
@@ -129,8 +122,8 @@ create table favorite_genre
 (
     genre_id int unsigned not null,
     user_id  int unsigned not null,
-    constraint `PRIMARY`
-        primary key (genre_id, user_id),
+
+    primary key (genre_id, user_id),
     constraint favorite_genre_genre_genre_id_fk
         foreign key (genre_id) references genre (genre_id),
     constraint favorite_genre_user_user_id_fk
@@ -140,7 +133,6 @@ create table favorite_genre
 create table quotes
 (
     quote_id      int unsigned auto_increment
-        constraint `PRIMARY`
         primary key,
     user_id       int unsigned                not null,
     author_id     int unsigned                not null,
@@ -161,8 +153,8 @@ create table user_books
     added_date    datetime default CURRENT_TIMESTAMP not null,
     finished_date datetime default CURRENT_TIMESTAMP null,
     rating        int                                null,
-    constraint `PRIMARY`
-        primary key (user_id, book_id),
+
+    primary key (user_id, book_id),
     constraint user_books_book_id_fk
         foreign key (book_id) references book (id),
     constraint user_books_status_status_id_fk
@@ -184,8 +176,8 @@ create table user_friends
     user1_id   int unsigned                       not null,
     user2_id   int unsigned                       not null,
     added_date datetime default CURRENT_TIMESTAMP not null,
-    constraint `PRIMARY`
-        primary key (user1_id, user2_id),
+
+    primary key (user1_id, user2_id),
     constraint user_friends_user_user_id_fk
         foreign key (user1_id) references user (user_id),
     constraint user_friends_user_user_id_fk2
@@ -195,7 +187,6 @@ create table user_friends
 create table user_reviews
 (
     review_id   int unsigned auto_increment
-        constraint `PRIMARY`
         primary key,
     user_id     int unsigned                 not null,
     book_id     int unsigned                 not null,
