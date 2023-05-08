14d9ec1dc54055ab8c415bb55a364b027a2ae157
ChisKien <chiskien03214@gmail.com>
2023-05-08 21:16:07 +0700

    ExcellentReads Project Initialization

---
+++ src/main/java/com/excellent_reads/ExcellentReadsApplication.java
@@ -0,0 +1,13 @@
+package com.excellent_reads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcellentReadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcellentReadsApplication.class, args);
+    }
+
+}
