2d6c0b8d36c10247aabec7462c64ad4a569d6350
ChisKien <chiskien03214@gmail.com>
2023-05-12 14:41:35 +0700

    get book by id

--- src/main/java/com/excellent_reads/models/Publisher.java
+++ src/main/java/com/excellent_reads/models/Publisher.java
@@ -1,4 +1,12 @@
 package com.excellent_reads.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Publisher {
+    private Set<Book> books = new HashSet<>();
+    private Long publisherId;
+    private String name;
+    private String homepage;
+    private String country;
 }
